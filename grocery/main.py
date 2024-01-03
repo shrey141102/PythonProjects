7a842ccdf79aed5d2ce4fe632a77875358540814
ogsahil <contactsahilpnayak@gmail.com>
2024-01-03 23:08:28 +0530

    In progress

--- grocery/main.py
+++ grocery/main.py
@@ -1,3 +1,5 @@
+AvailableProductDict = {}
+
 #welcome message for the customer
 CustomerName = input("Enter your name: ")
 WelcomeMessage = f"welcome to our store {CustomerName}"
@@ -15,6 +17,27 @@ file_lines = Product_file.readlines()
 Product_file.close()
 
 #iterate one by one all products present in the text file
+print("*************** items present in the store ******************")
 
 for i in file_lines:
-    print(i)
+    item_name = i.split()[0]
+    item_price = i.split()[1]
+    print(f"{item_name} : {item_price}")
+    AvailableProductDict.update({item_name : item_price})
+
+# print(AvailableProductDict)
+    
+
+print("*************** items present in the store ******************")
+
+
+#getting permission from the user whether they want to proceed or not
+
+ProceedMsg = input("Do you want to proceed? (Y/N)")
+
+if ProceedMsg.lower() == "yes":
+    itemEntered = input("Enter the item that you want to add to your cart: ")
+    if itemEntered.title() in AvailableProductDict:
+
+else:
+    print("Please come back again!!!")
